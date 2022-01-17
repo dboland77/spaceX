803d0969f1e5f4b51b164a98b036e426d3cfc4b9
Dave Boland <dboland77@gmail.com>
2022-01-17 17:06:04 +0000

    Adding lint staged, jest to pre-commit, finishing dev setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,60 @@
 const defaultRules = [
-  "react-app",
-  "airbnb",
-  "airbnb/hooks",
-  "eslint:recommended",
-  "plugin:jest/recommended",
-  // any other plugins or custom configuration you'd like to extend from.
+    'react-app',
+    'airbnb',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:jest/recommended',
+    // any other plugins or custom configuration you'd like to extend from.
 ];
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
     },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-    jest: true,
-  },
-  extends: [...defaultRules, "prettier", "prettier/react"],
-  rules: {
-    "array-callback-return": "warn",
-    "consistent-return": "warn",
-    "default-case": "warn",
-  },
-  overrides: [
-    {
-      files: ["**/*.ts", "**/*.tsx"],
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-      plugins: ["@typescript-eslint"],
-      extends: [
-        ...defaultRules,
-        "plugin:@typescript-eslint/recommended",
-        "airbnb-typescript",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/react",
-      ],
-      rules: {
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "@typescript-eslint/no-unused-expressions": "warn",
-        "import/no-extraneous-dependencies": [
-          "error",
-          { devDependencies: ["**/setupTests.ts", "**/**.test.tsx"] },
-        ],
-      },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true,
     },
-  ],
-  settings: {
-    react: {
-      version: "detect",
+    extends: [...defaultRules, 'prettier'],
+    rules: {
+        'array-callback-return': 'warn',
+        'consistent-return': 'warn',
+        'default-case': 'warn',
     },
-  },
+    overrides: [
+        {
+            files: ['**/*.ts', '**/*.tsx'],
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+            plugins: ['@typescript-eslint'],
+            extends: [
+                ...defaultRules,
+                'plugin:@typescript-eslint/recommended',
+                'airbnb-typescript',
+                'prettier',
+            ],
+            rules: {
+                '@typescript-eslint/no-explicit-any': 'warn',
+                '@typescript-eslint/no-unused-vars': 'warn',
+                '@typescript-eslint/no-unused-expressions': 'warn',
+                'import/no-extraneous-dependencies': [
+                    'error',
+                    { devDependencies: ['**/setupTests.ts', '**/**.test.tsx'] },
+                ],
+            },
+        },
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 };
